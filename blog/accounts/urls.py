15f5eed960c2fa0c87642b3163e4bb6ff1e98374
Davide Pedranz <davide.pedranz@gmail.com>
2015-12-19 15:47:30 +0100

    simple account handling

--- blog/accounts/urls.py
+++ blog/accounts/urls.py
@@ -1,6 +1,5 @@
-from django.conf.urls import url
-from . import views
+from django.conf.urls import include, url
 
 urlpatterns = [
-    url(r'login', views.login, name='login'),
+    url('^', include('django.contrib.auth.urls'))
 ]
