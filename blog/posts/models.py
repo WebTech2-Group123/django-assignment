b2ec043a8e93e5f06696eaa32ca1f162aefef754
Davide Pedranz <davide.pedranz@gmail.com>
2015-12-20 23:09:58 +0100

    Add str to model

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -7,3 +7,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     text = models.TextField(max_length=500)
     user = models.ForeignKey(User)
+
+    # useful for the administration panel
+    def __str__(self):
+        return self.title
