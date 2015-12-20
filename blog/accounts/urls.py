aeb91e6c088cafecf7fdd072a44697d65094b455
Davide Pedranz <davide.pedranz@gmail.com>
2015-12-20 12:43:41 +0100

    Configure profile links

--- blog/accounts/urls.py
+++ blog/accounts/urls.py
@@ -1,9 +1,16 @@
 from django.conf.urls import url, include
+from .views import profile
 
 urlpatterns = [
 
     # login
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'accounts/login.html'}),
+
+    # logout
+    url(r'^logout/$', 'django.contrib.auth.views.logout_then_login'),
+
+    # profile
+    url(r'^profile/$', profile, name='profile'),
 
     # registration, activation
     url(r'^', include('registration.backends.hmac.urls')),
