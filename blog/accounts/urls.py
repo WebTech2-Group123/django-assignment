ddb8f545a8cf48606df22d8d0ce64078ea37952d
Davide Pedranz <davide.pedranz@gmail.com>
2015-12-20 11:27:01 +0100

    Use django-registration

--- blog/accounts/urls.py
+++ blog/accounts/urls.py
@@ -1,8 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.core.urlresolvers import reverse_lazy
 from .views import SignUpView, signup_ok
 
 urlpatterns = [
+
+    url(r'^', include('registration.backends.hmac.urls')),
 
     # signup
     url(r'^signup/$', SignUpView.as_view(success_url=reverse_lazy('signup-ok')), name='signup'),
